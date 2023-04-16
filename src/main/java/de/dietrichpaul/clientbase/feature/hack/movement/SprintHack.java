5c0fd17d2655e523bea39ba8be3d303573ad5f30
DietrichPaul <dietrich.paul@protonmail.com>
2023-04-16 23:50:37 +0200

    removed test property

--- src/main/java/de/dietrichpaul/clientbase/feature/hack/movement/SprintHack.java
+++ src/main/java/de/dietrichpaul/clientbase/feature/hack/movement/SprintHack.java
@@ -14,15 +14,12 @@ package de.dietrichpaul.clientbase.feature.hack.movement;
 import de.dietrichpaul.clientbase.event.KeyPressedStateListener;
 import de.dietrichpaul.clientbase.feature.hack.Hack;
 import de.dietrichpaul.clientbase.feature.hack.HackCategory;
-import de.dietrichpaul.clientbase.property.impl.BooleanProperty;
 import de.dietrichpaul.clientbase.ClientBase;
 
 public class SprintHack extends Hack implements KeyPressedStateListener {
-    private final BooleanProperty allDirection = new BooleanProperty("AllDirection", false);
 
     public SprintHack() {
         super("Sprint", HackCategory.MOVEMENT);
-        addProperty(allDirection);
     }
 
     @Override

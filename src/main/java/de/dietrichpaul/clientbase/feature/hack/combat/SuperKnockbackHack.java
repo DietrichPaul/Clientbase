091f1d1aac4154db95b3030c4e041d2b6df14bd8
DietrichPaul <dietrich.paul@protonmail.com>
2023-05-15 20:55:05 +0200

    improved wtap

--- src/main/java/de/dietrichpaul/clientbase/feature/hack/combat/SuperKnockbackHack.java
+++ src/main/java/de/dietrichpaul/clientbase/feature/hack/combat/SuperKnockbackHack.java
@@ -6,15 +6,23 @@ import de.dietrichpaul.clientbase.event.TargetPickListener;
 import de.dietrichpaul.clientbase.event.UpdateListener;
 import de.dietrichpaul.clientbase.feature.hack.Hack;
 import de.dietrichpaul.clientbase.feature.hack.HackCategory;
+import de.dietrichpaul.clientbase.property.impl.IntProperty;
+import de.dietrichpaul.clientbase.util.math.MathUtil;
 import de.dietrichpaul.clientbase.util.minecraft.ChatUtil;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.text.Text;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
 
 public class SuperKnockbackHack extends Hack implements KeyPressedStateListener, UpdateListener {
 
+    private final IntProperty hurtTimeProperty = new IntProperty("HurtTime", 7, 0, 10);
+
     public SuperKnockbackHack() {
         super("SuperKnockback", HackCategory.COMBAT);
+        addProperty(hurtTimeProperty);
     }
 
     @Override
@@ -39,11 +47,25 @@ public class SuperKnockbackHack extends Hack implements KeyPressedStateListener,
         boolean canSprint = mc.options.sprintKey.isPressed();
         Entity target = ClientBase.INSTANCE.getEventDispatcher()
                 .post(new TargetPickListener.TargetPickEvent(null)).getTarget();
-        if (releaseW == 0 && canSprint && event.pressed && target instanceof LivingEntity living && living.hurtTime == 10) {
-            releaseW = 1;
-        }
-        if (releaseW != 0) {
-            event.pressed = false;
+        if (target instanceof LivingEntity living) {
+            Vec3d camera = mc.player.getCameraPosVec(1.0F);
+            Box dimension = living.getBoundingBox(EntityPose.STANDING);
+            Box now = dimension.offset(target.getPos());
+            Box nextTick = dimension.offset(living.serverX, living.serverY, living.serverZ);
+
+            Vec3d closestNow = MathUtil.clamp(camera, now);
+            Vec3d closestNextTick = MathUtil.clamp(camera, nextTick);
+
+            boolean isNextTickFurtherAway = camera.distanceTo(closestNextTick)
+                    > camera.distanceTo(closestNow);
+
+            if (releaseW == 0 && canSprint && event.pressed && living.hurtTime >= hurtTimeProperty.getValue() && isNextTickFurtherAway) {
+                releaseW = 1;
+                ChatUtil.sendChatMessage(Text.of("WTAP"));
+            }
+            if (releaseW != 0) {
+                event.pressed = false;
+            }
         }
     }
 
